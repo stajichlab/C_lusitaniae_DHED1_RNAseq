3919cc7d7a7a9e5bc08b760b0ecf7ff3bf6be1ac
Jason Stajich <jasonstajich.phd@gmail.com>
2017-12-19 15:23:55 -0800

    gsnap updated testing run

---
+++ jobs/01_gsnap.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/bash
+#SBATCH --nodes 1 --ntasks 16 --mem 16gb --time 12:00:00 -J gmap --out logs/gmap.%a.log
+
+module load gmap
+
+GENOME=genome/candida_lusitaniae_ATCC42720_w_CBS_6936_MT
+CPU=2
+THREADCOUNT=1
+if [ $SLURM_CPUS_ON_NODE ]; then
+ CPU=$SLURM_CPUS_ON_NODE
+ THREADCOUNT=$(expr $CPU - 2)
+ if [ $THREADCOUNT -lt 1 ]; then
+  THREADCOUNT=1
+ fi
+fi
+
+N=${SLURM_ARRAY_TASK_ID}
+INDIR=raw
+OUTDIR=aln
+SAMPLEFILE=samples.txt
+mkdir -p $OUTDIR
+
+if [ ! $N ]; then
+ N=$1
+fi
+
+if [ ! $N ]; then
+ echo "cannot run without a number provided either cmdline or --array in sbatch"
+ exit
+fi
+IFS=,
+sed -n ${N}p $SAMPLEFILE | while read FOLDER SAMPLE REP
+do
+ echo $FOLDER $SAMPLE
+ 
+ OUTFILE=$OUTDIR/${SAMPLE}.r${REP}.gsnap.sam
+ READS=$(ls $INDIR/${FOLDER}/*.fastq.gz | perl -p -e 's/\s+/ /g')
+ echo "$READS"
+ if [ ! -f $OUTFILE ]; then  
+  echo "module load gmap" > job_$FOLDER.sh
+  echo "gsnap -t $THREADCOUNT -s splicesites -D genome --gunzip \
+ -d candida_lusitaniae --read-group-id=$FOLDER --read-group-name=$SAMPLE \
+ -A sam $READS > $OUTFILE" >> job_$SAMPLE.sh
+ bash job_$FOLDER.sh
+ unlink job_$FOLDER.sh
+ fi
+
+done
