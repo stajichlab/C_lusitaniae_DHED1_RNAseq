37a9705908eba19e638cf97d380edccae097465d
Jason Stajich <jasonstajich.phd@gmail.com>
2017-12-19 15:24:32 -0800

    string tie to raw counts for DEseq

---
+++ jobs/04_get_counts.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+#SBATCH --nodes 1 --ntasks 2 --mem 4gb --time 24:00:00 --out logs/prep_DEseq.log
+module load stringtie
+PREFIX=DHED1
+OUT=reports
+mkdir -p $OUT
+cd results
+for dir in *.r1
+do
+ pushd $dir;
+ ln -s ../$(basename `pwd`)*.gtf .
+ popd
+done
+cd ..
+prepDE.py -g $OUT/$PREFIX.DESeq.gene_count.tab -t $OUT/$PREFIX.DESeq.trans_count.tab -i results
