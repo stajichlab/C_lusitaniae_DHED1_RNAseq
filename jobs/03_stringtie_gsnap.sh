058d6266608067369a0f935c3ad5f55b0ef57b7c
Jason Stajich <jasonstajich.phd@gmail.com>
2017-12-19 16:16:10 -0800

    go ahead and stringtie gsnap results too

---
+++ jobs/03_stringtie_gsnap.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+#SBATCH --nodes 1 --ntasks 24 --mem 16G 
+#SBATCH-J stringtie.estimate --out logs/stringtie.%a.log --time 12:00:00
+
+module load stringtie
+N=${SLURM_ARRAY_TASK_ID}
+OUTDIR=results
+INDIR=aln
+SAMPLEFILE=samples.txt
+GTF=genome/candida_lusitaniae_1_transcripts.gtf
+CPU=1
+if [ $SLURM_CPUS_ON_NODE ]; then
+ CPU=$SLURM_CPUS_ON_NODE
+fi
+mkdir -p $OUTDIR
+if [ ! $N ]; then
+ N=$1
+fi
+
+if [ ! $N ]; then
+ echo "cannot run without a number provided either cmdline or --array in qsub"
+ exit
+fi
+
+IFS=,
+sed -n ${N}p $SAMPLEFILE | while read FOLDER SAMPLE REP
+do
+ echo "$FOLDER $SAMPLE"
+ INFILE=$INDIR/${SAMPLE}.r${REP}.gsnap.bam
+ echo "NAME is $SAMPLE REP is $REP infile = $INFILE OUTDIR=$OUTDIR/${SAMPLE}.r${REP} " 
+ mkdir -p $OUTDIR/${NAME}.r${REP}
+ stringtie --fr -p $CPU -G $GTF -b $OUTDIR/${SAMPLE}.r${REP}.gsnap -e -o $OUTDIR/${SAMPLE}.r${REP}.gsnap.stringtie.gtf $INFILE
+done
